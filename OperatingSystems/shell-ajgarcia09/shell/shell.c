9903f1cdbbea7091051e2d0c92d4ec4f259f2b83
Ana Garcia <ajgarciaramirez@miners.utep.edu>
2017-10-26 13:26:11 -0600

    fork fix

--- OperatingSystems/shell-ajgarcia09/shell/shell.c
+++ OperatingSystems/shell-ajgarcia09/shell/shell.c
@@ -3,11 +3,12 @@
 #include "shell.h"
 #include <sys/types.h>//for read
 #include <unistd.h>//for read
-#include <sys/stat.h>//for stat
 #include <fcntl.h> //for read
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+
+
 
 #define BUFLEN 1024
 
@@ -19,16 +20,11 @@ void freeTokens(char ** tokenVec){
  }
 
 char * concatStrings(char * str1, char * str2){
-  printf("entered concatStrings\n");
-  printf("str1: %s\n",str1);
-  printf("str2: %s\n", str2);
-  int newStringSize =lengthOfTokenArray(str1)+lengthOfTokenArray(str2)+2;
-  printf("newStringSize %d\n", newStringSize);
+  int newStringSize =lengthOfCharArray(str1)+lengthOfCharArray(str2)+2;
   char * newStr = malloc(newStringSize);
   int i =0;  
   while(str1[i]){
     newStr[i] = str1[i];
-    printf("current newStr: %c\n", newStr[i]);
     i++;
   }
   newStr[i] = '/'; 
@@ -36,18 +32,14 @@ char * concatStrings(char * str1, char * str2){
   int j =0;
   while(str2[j]){
     newStr[i] = str2[j];
-    printf("current newStr: %c\n", newStr[j]);
     j++;
     i++;
   }
   newStr[i] = '\0';
-  printf("newStr: %s\n", newStr);
   return newStr;
 }
   
-    
 char ** getPathList(char ** envp){
-  printf("in getPath\n");
   char ** tokens; 
   for(int i = 0; envp[i]; i++){
     tokens = mytoc(envp[i], '=');
@@ -62,7 +54,7 @@ char ** getPathList(char ** envp){
   }
 
 char ** updateInputList(char ** inputList, char * command){
-  int commandSize = lengthOfTokenArray(command); //+ null char
+  int commandSize = lengthOfCharArray(command); //+ null char
   inputList[0] = '\0';
   inputList[0] = malloc(commandSize+1);
   int i =0;
@@ -73,19 +65,16 @@ char ** updateInputList(char ** inputList, char * command){
   return inputList;
 
 }
-    
-
-
-  /*argc, argv and envp are used to get environment
+/*argc, argv and envp are used to get environment
   variables used to find the PATH variable.*/
 
 int main(int argc, char **argv, char **envp){
-  for(;;){
+while(1){
     char input[BUFLEN];
     char * command;
     write(1,"[ajgarcia09 shell]$ ",19);
     int numBytesRead = read(0,input,BUFLEN);
-    input[numBytesRead-1] = '\0'; //remove new line char
+    input[numBytesRead-1] = '\0'; //remove new line char*/
     if(compStrings(input,"exit")==1){
       break;  //exit program
     }
@@ -93,15 +82,19 @@ int main(int argc, char **argv, char **envp){
    char ** inputList = mytoc(input, ' ');
    //absolute path was entered
    if(access(inputList[0], X_OK) == 0){
-     char * command = inputList[0];
-     printf("found command: %s\n",command);
-     execve(command,inputList,envp);
-     free(command);
+    char * command = inputList[0];
+    int pid =  fork();
+   if(pid ==0){
+      execve(command,inputList,envp);
+      free(command);
    }
+   else{
+     wait(NULL);
+   }
+}
    else{//search for command in path list
-     printf("didn't find command %s\n", inputList[0]);
      char ** path = getPathList(envp);
-     int sizeOfInputListIndex = lengthOfTokenArray(inputList[0]);
+     int sizeOfInputListIndex = lengthOfCharArray(inputList[0]);
      char * temp = (char*)malloc(sizeOfInputListIndex+1);
      for(int i = 0; i < sizeOfInputListIndex; i++){
        if(i == (sizeOfInputListIndex -1)){
@@ -111,27 +104,33 @@ int main(int argc, char **argv, char **envp){
       }
      
      for(int i =0; path[i]; i++){
-       command = concatStrings( path[i],temp);
-       printf("command: %s\n", command);
+       char * command = concatStrings( path[i],temp);
        inputList = updateInputList(inputList,command);
-       execve(command,inputList,envp);
-       free(command);
-       //printf("new inputList[0]: %s\n",inputList[0]);
-       // command2 = concatStrings(command,inputList[0]);
-       //printf("command2 %s\n",command2);
-    }
-
-  pid_t  pid = fork();
+int pid = fork();
+if(pid == 0){
+execve(command,inputList,envp);
+    free(command);
+     }
+ else{
+   wait(NULL);
+  }
+}
+printf("about to fork\n");
+int pid = fork();
+printf("forking with pid:%d\n",pid);
    if(pid == 0){ //child is running
-     execve(command, inputList,envp);
+       printf("child is running\n");
+       execve(command, inputList,envp);
+        }
+     else{
+       wait(NULL); //wait for the child to finish
+       printf("parent is running\n");
+       freeTokens(inputList);
+       free(inputList);
+     }
    }
-   else{
-     wait(NULL);
-     freeTokens(inputList);
-     free(inputList);
-   }
-  }
-  }
+  printf("End \n");
+}
 }
      
 
