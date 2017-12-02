eec36797b9a3073af810a8b042db1beb44454a9e
Ana Garcia <ajgarciaramirez@miners.utep.edu>
2017-12-01 19:03:06 -0700

    piping tokenizes correctly

--- shell/shellDemo.c
+++ shell/shellDemo.c
@@ -25,13 +25,14 @@ int main(int argc, char **argv, char **envp){
    if(checkForPipe(input)){
      printf("found pipe! :D \n");
      char ** pipeCommands = mytoc(input, '|');
-     //printf("pipeCommands[0]: %s\n", pipeCommands[0]);
-     //printf("pipecommands[1]: %s\n", pipeCommands[1]);
+     printf("pipeCommands[0]: %s\n", pipeCommands[0]);
+     printf("pipecommands[1]: %s\n", pipeCommands[1]);
      
-     //char ** commandOne = mytoc(pipeCommands[0], ' ');
-     //printf("commandOne[0]: %s\n", commandOne[0]);
-     //char ** commandTwo = mytoc(pipeCommands[1], ' ');
-     //printf("command
+     char ** commandOne = mytoc(pipeCommands[0], ' ');
+     printf("commandOne[0]: %s\n", commandOne[0]);
+     char ** commandTwo = mytoc(pipeCommands[1], ' ');
+     printf("commandTwo[0]: %s\n",commandTwo[0]);
+     printf("commandTwo[1]: %s\n", commandTwo[1]);
      //forkPipe(char ** commandOne, char ** commandTwo, char ** path, char **envp);
        
    }
